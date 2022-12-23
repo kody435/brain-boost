bf28a1d28d90dd4c8a36033276c4ba2f48e325c4
Cody <bittusmaths@gmail.com>
2022-12-23 23:37:47 +0530

    commit, new changes as always

--- pages/Ask_Ques.jsx
+++ pages/Ask_Ques.jsx
@@ -7,22 +7,22 @@ const Ask_Ques = () => {
             <div className='bg-gradient-to-bl from-sky-600 to-indigo-600 w-full h-full'>
                 <div className='flex flex-col items-center justify-center h-screen'>
                     <form className='h-96 w-96 items-center justify-center rounded-3xl ' >
-                        <table className='flex flex-col justify-center items-center '>
+                        <div className='flex flex-col justify-center items-center '>
                             <table className='flex flex-col items-center justify-center h-72'>
-                            <tr className='flex flex-row w-full items-center justify-center '>
-                                    <td className='text-xl mr-3'>Subject</td>
-                                    <td className='rounded-2xl'><input type="text" className='w-56 rounded-3xl px-4 py-2' /></td>
-                                </tr>
-                                <br></br>
-                            <tr className='flex flex-row w-full items-center justify-center '>
+                                <tr className='flex flex-row w-full items-center justify-center text-center'>
+                                        <td className='text-xl mr-6'>Subject</td>
+                                        <td className='rounded-2xl'><input type="text" className='text-xl w-56 lg:w-80 rounded-3xl px-4 py-2' /></td>
+                                    </tr>
+                                    <br></br>
+                                <tr className='flex flex-row w-full items-center justify-center'>
                                     <td className='text-xl mr-3'>Question</td>
-                                    <td className='rounded-2xl'><textarea type="text" className='w-full rounded-3xl px-4 py-2' /></td>
+                                    <td className='rounded-3xl text-xl'><textarea type="text" className='w-full lg:w-80 rounded-2xl px-4 py-2' /></td>
                                 </tr>
                             </table>
                             <div className='w-fit flex items-center justify-center py-3 text-center px-12 rounded-full  bg-black hover:bg-white text-white hover:text-black '>
                                 <button type='submit' className='py-2 text-center '>SUBMIT</button>
                             </div>
-                        </table>
+                        </div>
                         
                     </form>
                 </div>  
