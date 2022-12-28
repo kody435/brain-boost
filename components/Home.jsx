f037830a8aa14d81ad034f543927362fc110758f
Cody <bittusmaths@gmail.com>
2022-12-28 20:07:56 +0530

    Gridding

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from './common.module.css'
 
 const Home = () => {
 
@@ -6,30 +7,33 @@ const Home = () => {
     const users = [{ name: "John", score: 10 }, { name: "Jane", score: 20 }, { name: "Jack", score: 30 }, { name: "Jill", score: 40 }, { name: "Jenny", score: 50 }, { name: "Jenny", score: 60 }, { name: "Jenny", score: 70 }, { name: "Jenny", score: 80 }, { name: "Jenny", score: 90 }, { name: "Jenny", score: 100 }].reverse();
 
     return (
-        <div className='grid grid-cols-3'>
-            <div className='cols-span-2'>
-                <div className=' text-3xl flex justify-center py-10 text-white '><h2>Questions</h2></div>
+        <div className='grid grid-cols-4 mt-10'>
+
+            <div className={styles.grid1}>
+                <div className='text-3xl flex justify-center text-white '><h2>Questions</h2></div>
                 <div className='text-white mb-5 '>
                     {questions.map((question, index) => (
-                        <div key={index} className="m-4 flex flex-row space-x-4 p-4 text-xl font-medium bg-blue-700 rounded-xl">
+                        <div key={index} className="m-4 flex flex-row space-x-4 p-4 text-xl font-medium bg-blue-900 rounded-xl">
                             <div className='bg-transparent'>Question : {question.question}</div>
                             <div className=' rounded-xl text-center bg-white px-3 text-black'>Answer</div>
                         </div>
                     ))}
                 </div>
             </div>
-            <div className='cols-span-1'>
-                <div className='text-3xl flex justify-center py-10 text-white '><h2>Leaderboard</h2>
-                </div>
+
+            
+            <div className={styles.grid2}>
+                <div className='text-3xl mb-14 flex justify-center text-white '><h2>Leaderboard</h2></div>
                 <div className='text-white mb-5 '>
                     {users.map((user, index) => (
-                        <div key={index} className="m-4 flex flex-row space-x-4 p-4 text-xl font-medium bg-blue-700 rounded-xl">
+                        <div key={index} className="m-4 flex flex-row space-x-4 p-4 text-xl font-medium bg-blue-900 rounded-xl">
                             <div className='bg-transparent'>Name : {user.name}</div>
                             <div className=' rounded-xl text-center bg-white px-3 text-black'>Score : {user.score}</div>
                         </div>
                     ))}
                 </div>
             </div>
+
         </div>
     )
 }
