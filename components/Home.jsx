611658ae0d7e8b8b4a6a4240c253563308acf5e5
Cody <bittusmaths@gmail.com>
2023-01-05 09:26:48 +0530

    Update Home.jsx

--- components/Home.jsx
+++ components/Home.jsx
@@ -9,6 +9,7 @@ const contractTxId = "sPyXyPDKw9uKFs43y7HFvsnKUE7bht3DkBNKA5UcV_o";
 
 const Home = () => {
     const [Questions, setQuestions] = useState([])
+    const [Users, setUsers] = useState([])
     const [initDB, setInitDB] = useState(false)
     let questions = useRef()
 
@@ -29,9 +30,14 @@ const Home = () => {
         setQuestions(await db.cget("Questions", ["title"]))
     }
 
+    const getUsers = async () => {
+        setUsers(await db.cget("Questions", ["user_address"]))
+    }
+
     useEffect(() => {
         if (initDB) {
             getQuestions()
+            getUsers()
         }
     }, [initDB])
 
@@ -53,10 +59,12 @@ const Home = () => {
             <div className={styles.grid2}>
                 <h2 className='text-3xl bg-zinc-900 flex justify-center p-screen bg-clip-text text-transparent bg-gradient-to-r from-green-600 to-violet-600'>Leaderboard</h2>
                 <div className='mb-5 '>
+                    {map(v => (
                     <div className="bg-white flex justify-center flex-row m-4 space-x-4 p-4 text-xl font-medium rounded-xl drop-shadow-xl bg-gradient-to-r from-violet-700 to-fuchsia-800">
-                        <div className='bg-transparent text-white'></div>
-                        <h2 className='bg-transparent text-white rounded-xl'>Score</h2>
+                        <div className='bg-transparent text-white'>Score :</div>
+                            <h2 className='bg-transparent text-white rounded-xl'>{v.data.user_address}</h2>
                     </div>
+                    ))(Users)}
                 </div>
             </div>
 
