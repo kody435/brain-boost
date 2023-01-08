93a12a4149848052e28f65830ad66918d923c66e
Cody <bittusmaths@gmail.com>
2023-01-08 14:52:05 +0530

    Console logging of data done. Now sending it to WeaveDB

--- pages/Ask_Ques.jsx
+++ pages/Ask_Ques.jsx
@@ -8,8 +8,8 @@ const contractTxId= "sPyXyPDKw9uKFs43y7HFvsnKUE7bht3DkBNKA5UcV_o"
 
 const Ask_Ques = () => {
     const [user, setUser] = useState([]);
-    let title = useRef()
-    let question = useRef()
+    const [titles, setTitles] = useState("");
+    const [questions, setQuestions] = useState("");
     const [initDB, setInitDB] = useState(false)
 
     const setupWeaveDB = async () => {
@@ -20,7 +20,6 @@ const Ask_Ques = () => {
         await db.initializeWithoutWallet()
         setInitDB(true)
     }
-
     useEffect(() => {
         setupWeaveDB()
     }, [])
@@ -34,10 +33,8 @@ const Ask_Ques = () => {
 		} else {
 			console.log("We have the ethereum object", ethereum);
 		}
-
         const accounts = await ethereum.request({ method: "eth_accounts" });
         setUser(accounts)
-
 		if (accounts.length !== 0) {
 			const account = accounts[0];
 			console.log("Found an authorized account:", account);
@@ -51,8 +48,9 @@ const Ask_Ques = () => {
 		checkIfWalletIsConnected();
     }, []);
     
-    const addQuestion = async(title, question) => {
-        await db.add({ title, question, user_address: db.signer() }, "Questions")
+    const addQuestion = (e) => {
+        e.preventDefault()
+        console.log(titles, questions)
     }
     
     return (
@@ -63,27 +61,23 @@ const Ask_Ques = () => {
                 <h1 className='text-center items-center text-3xl  bg-clip-text text-transparent bg-gradient-to-l from-green-600 to-blue-500 '>Submit question here</h1>
                     <div className='h-96 w-96 items-center justify-center rounded-3xl '>
                         <div className='flex flex-col justify-center items-center '>
-                            <table className='flex flex-col items-center justify-center h-72'>
+                            <div className='flex flex-col items-center justify-center h-72'>
                                 <tr className='flex flex-row w-full items-center justify-center text-center '>
                                     <td className='text-xl mr-6 text-black '>Title</td>
                                     <td className='rounded-2xl'>
-                                        <input type="text" className='text-xl w-56 lg:w-80 rounded-3xl px-4 py-2 bg-white' placeholder="Write the shortest title as per question" value={title.current} />
+                                        <input type="text" className='text-xl w-56 lg:w-80 rounded-3xl px-4 py-2 bg-white' placeholder="Write the shortest title as per question" value={titles} onChange={(e) => {setTitles(e.target.value)}} />
                                     </td>
                                 </tr>
                                 <br></br>
                                 <tr className='flex flex-row w-full items-center justify-center'>
                                     <td className='text-xl mr-3 text-black '>Question</td>
                                     <td className='rounded-3xl text-xl'>
-                                        <textarea type="text" className='w-full lg:w-80 rounded-2xl px-4 py-2 bg-white' placeholder='Explain the whole question in detail here' value={question.current} />
+                                        <textarea type="text" className='w-full lg:w-80 rounded-2xl px-4 py-2 bg-white' placeholder='Explain the whole question in detail here' value={questions} onChange={(e) => {setQuestions(e.target.value)}} />
                                     </td>
                                 </tr>
-                            </table>
+                            </div>
                             <div className='w-fit flex items-center justify-center font-bold py-5 text-center px-12 rounded-full border-2 border-white hover:bg-black hover:border-blue-700 bg-white text-black hover:text-white duration-700'
-                                onClick={
-                                    async () => {
-                                        await addQuestion(title.current, question.current)
-                                    }
-                                }
+                                onClick={addQuestion}
                             >
                                 SUBMIT
                             </div>
