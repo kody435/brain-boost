d771d9102094225f05686e5e4e16c9de8068eb01
Cody <bittusmaths@gmail.com>
2023-01-08 14:32:13 +0530

    Update Home.jsx

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,24 +1,16 @@
-import styles from './common.module.css'
-import { useEffect, useRef, useState } from "react"
-import { Buffer } from "buffer"
-import { map } from "ramda"
+import React,{useState , useEffect, useRef} from 'react'
+import styles from "../components/common.module.css"
+import WeaveDB from "weavedb-sdk"
 import SDK from 'weavedb-sdk'
 
-let db;
-const contractTxId= "sPyXyPDKw9uKFs43y7HFvsnKUE7bht3DkBNKA5UcV_o";
+let db
+const contractTxId= "sPyXyPDKw9uKFs43y7HFvsnKUE7bht3DkBNKA5UcV_o"
 
-const Home = () => {
-    const [Questions, setQuestions] = useState([])
-    const [Users, setUsers] = useState([])
+const Ask_Ques = () => {
+    const [user, setUser] = useState([]);
+    const [titles, setTitles] = useState("");
+    const [questions, setQuestions] = useState("");
     const [initDB, setInitDB] = useState(false)
-
-    const getQuestions = async () => {
-        setQuestions(await db.cget("Questions", ["title"]))
-    }
-
-    const getUsers = async () => {
-        setUsers(await db.cget("Questions", ["user_address"]))
-    }
 
     const setupWeaveDB = async () => {
         window.Buffer = Buffer
@@ -31,42 +23,74 @@ const Home = () => {
     useEffect(() => {
         setupWeaveDB()
     }, [])
+    
+    const checkIfWalletIsConnected = async () => {
+		const { ethereum } = window;
+
+		if (!ethereum) {
+			console.log("Make sure you have metamask!");
+			return;
+		} else {
+			console.log("We have the ethereum object", ethereum);
+		}
+
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+        setUser(accounts)
+
+		if (accounts.length !== 0) {
+			const account = accounts[0];
+			console.log("Found an authorized account:", account);
+			setUser(account);
+		} else {
+			console.log("No authorized account found");
+		}
+    };
 
     useEffect(() => {
-        if (initDB) {
-            getQuestions()
-            getUsers()
-        }
-    }, [initDB])
-
+		checkIfWalletIsConnected();
+    }, []);
+    
+    const addQuestion = async (e) => {
+        e.preventDefault()
+        await db.add({ title: titles, question: questions, user_address: db.signer() }, "Questions")
+        console.log(titles, questions)
+    }
+    
     return (
-        <div className='lg:grid flex flex-col lg:grid-cols-7 mt-10 '>
-
-            <div className={styles.grid1}>
-                <h2 className='bg-clip-text text-transparent bg-gradient-to-r from-sky-600 to-violet-600 text-3xl flex justify-center mb-5'>Questions</h2>
-                <div className='text-white mb-5 '>
-                    {map(v => (
-                        <div className="text-white mx-8 my-2 space-y-4 flex flex-row justify-start items-center space-x-4 p-5 text-xl font-medium bg-gradient-to-l from-sky-600 to-indigo-600 rounded-xl drop-shadow-xl">
-                            <div className='bg-transparent'>{v.data.title}</div>
-                        </div>
-                    ))(Questions)}
-                </div>
+        <div className={styles.quest}>
+            <title>Brain Boost</title>
+            <div className='bg-gradient-to-bl from-sky-500 to-indigo-600 w-screen h-screen'>
+                <div className='flex flex-col items-center justify-center h-screen'>
+                <h1 className='text-center items-center text-3xl  bg-clip-text text-transparent bg-gradient-to-l from-green-600 to-blue-500 '>Submit question here</h1>
+                    <div className='h-96 w-96 items-center justify-center rounded-3xl '>
+                        <div className='flex flex-col justify-center items-center ' >
+                            <table className='flex flex-col items-center justify-center h-72'>
+                                <tr className='flex flex-row w-full items-center justify-center text-center '>
+                                    <td className='text-xl mr-6 text-black '>Title</td>
+                                    <td className='rounded-2xl'>
+                                        <input type="text" required className='text-xl w-56 lg:w-80 rounded-3xl px-4 py-2 bg-white' placeholder="Write the shortest title as per question" value={titles} onChange={(e) => setTitles(e.target.value)} />
+                                    </td>
+                                </tr>
+                                <br></br>
+                                <tr className='flex flex-row w-full items-center justify-center'>
+                                    <td className='text-xl mr-3 text-black '>Question</td>
+                                    <td className='rounded-3xl text-xl'>
+                                        <textarea type="text" required className='w-full lg:w-80 rounded-2xl px-4 py-2 bg-white' placeholder='Explain the whole question in detail here' value={questions} onChange={(e) => setQuestions(e.target.value)} />
+                                    </td>
+                                </tr>
+                            </table>
+                            <button className='w-fit flex items-center justify-center font-bold py-5 text-center px-12 rounded-full border-2 border-white hover:bg-black hover:border-blue-700 bg-white text-black hover:text-white duration-700'
+                                type='submit'
+                                onClick={addQuestion}
+                            >
+                                SUBMIT
+                            </button>
+                        </div>  
+                    </div>
+                </div>  
             </div>
-
-            
-            <div className={styles.grid2}>
-                <h2 className='text-3xl bg-zinc-900 flex justify-center p-screen bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-violet-600'>Leaderboard</h2>
-                <div className='mb-5 '>
-                    {map(v => (
-                        <div className="bg-white flex justify-center flex-row m-4 space-x-4 px-4 py-2 text-xl font-medium rounded-2xl drop-shadow-xl bg-gradient-to-r from-violet-700 to-fuchsia-800">
-                            <h2 className='bg-transparent text-white rounded-xl'>{v.data.user_address.substring(0,5)}...{v.data.user_address.substring(v.data.user_address.length-5)}</h2>
-                        </div>
-                    ))(Users)}
-                </div>
-            </div>
-
         </div>
     )
 }
 
-export default Home
+export default Ask_Ques
