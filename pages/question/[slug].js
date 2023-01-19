14c77cd28f6b2ca50b1d0d4b5c3b8f5f687838f8
Kody <bittusmaths@gmail.com>
2023-01-19 18:05:51 +0530

    Update [slug].js

--- pages/question/[slug].js
+++ pages/question/[slug].js
@@ -1,18 +1,24 @@
-// import SDK from "weavedb-node-client";
-import WeaveDB from "weavedb-node-client";
+import SDK from "weavedb-node-client";
 
 export async function getServerSideProps({ query }) {
-  // const db = new SDK({
-  //   contractTxId: "NK6_OUCdQo3RbXMjBFJXIyZPBa4mgeIr_8bwHPCPtp8",
-  //   rpc: "localhost:8080", // your.remote.node:443
-  // });
-  const db = new WeaveDB({
+  const db = new SDK({
     contractTxId: "sPyXyPDKw9uKFs43y7HFvsnKUE7bht3DkBNKA5UcV_o",
-    rpc: "localhost:8080",
+    rpc: "localhost:9090",
   });
   return {
-    props: { question: await db.get("Questions", query.id) },
+    props: {
+      question: (
+        await db.get("Questions", ["slug"], ["slug", "=", query.slug])
+      )[0],
+    },
   };
 }
 
-export default function Question({ question }) {}
+export default function Question({ question }) {
+  return (
+    <div>
+      <div>{question.title}</div>
+      <div>{question.question}</div>
+    </div>
+  );
+}
